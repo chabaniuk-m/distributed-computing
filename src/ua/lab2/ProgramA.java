fe6450148ea58fcd14873a86e7c299763d2a041b
chabanyukm <chabanyuk.maks@gmail.com>
2022-09-16 14:44:51 +0300

    lab 2 production

--- src/ua/lab2/ProgramA.java
+++ src/ua/lab2/ProgramA.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class  ProgramA {
     public static void main(String[] args) {
-        int n = 5;
-        Forest forest = new Forest(n);
+        int n = 3;
+        Forest forest = new Forest(14);
         SwarmOfBees[] swarm = new SwarmOfBees[n];
         for (int i = 0; i < n; i++) {
             swarm[i] = new SwarmOfBees(i + 1, forest);
@@ -36,10 +36,11 @@ class ForestArea {
 class Forest {
     private ForestArea[] forestAreas;
     private int areaIndex = 0;
+    private boolean isWinnieThePoohFound = false;
 
     public Forest(int numberOfAreas) {
         forestAreas = new ForestArea[numberOfAreas];
-        int idx = new Random().nextInt(numberOfAreas);
+        int idx = new Random(System.currentTimeMillis()).nextInt(numberOfAreas);
         for (int i = 0; i < numberOfAreas; i++) {
             forestAreas[i] = idx == i ? new ForestArea(i + 1, true) :
                     new ForestArea(i + 1, false);
@@ -52,6 +53,14 @@ class Forest {
         else
             return forestAreas[areaIndex++];
     }
+
+    public void setWinnieThePoohFound(boolean winnieThePoohFound) {
+        isWinnieThePoohFound = winnieThePoohFound;
+    }
+
+    public boolean isWinnieThePoohFound() {
+        return isWinnieThePoohFound;
+    }
 }
 
 class SwarmOfBees extends Thread {
@@ -63,21 +72,26 @@ class SwarmOfBees extends Thread {
         this.forest = forest;
     }
 
+    private void goHome() {
+        System.out.println("Зграє номер " + swarmNumber + " повертається до вулика");
+    }
+
     @Override
     public void run() {
-        ForestArea area = null;
-        synchronized (forest) {
-            area = forest.nextArea();
-            if (area == null) return;
-        }
-        if (area != null) {
+        ForestArea area;
+        while (!forest.isWinnieThePoohFound()) {
+            synchronized (forest) {
+                area = forest.nextArea();
+                if (area == null) break;
+            }
             System.out.println("Зграє номер " + swarmNumber + " прочісує ділянку номер " + area.getAreaNumber());
             if (area.hasWinnieThePooh()) {
                 System.out.println("Зграє номер " + swarmNumber + " знайшла та покарала Вінні Пуха");
+                forest.setWinnieThePoohFound(true);
             } else {
                 System.out.println("Зграє номер " + swarmNumber + " не знайшла Вінні Пуха");
             }
-            System.out.println("Зграє номер " + swarmNumber + " повертається до вулика");
+            goHome();
         }
     }
 }
