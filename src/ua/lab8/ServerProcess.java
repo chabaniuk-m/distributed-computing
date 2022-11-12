98c2a3308fc4d47b050e960f17cbda64c5023404
chabanyukm <chabanyuk.maks@gmail.com>
2022-11-12 21:46:40 +0200

    starting point for ActiveMQ server added

--- src/ua/lab8/ServerProcess.java
+++ src/ua/lab8/ServerProcess.java
@@ -1,14 +1,42 @@
 package ua.lab8;
 
-import ua.lab8.server.Controller;
+import ua.lab8.server.AMQServer;
 import ua.lab8.server.DAO;
+import ua.lab8.server.RMIServer;
 import ua.lab8.server.SocketServer;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerProcess {
     public static void main(String[] args) {
-        SocketServer server = new SocketServer();
         DAO dao = new DAO();
-        Controller controller = new Controller(dao, server);
-        controller.begin();
+        jmsServer(dao);
+    }
+
+    private static void socketServer(DAO dao) {
+        new SocketServer(dao);
+    }
+
+    private static void rmiServer(DAO dao) {
+        try {
+            final RMIServer server = new RMIServer(dao);
+            final Registry registry = LocateRegistry.createRegistry(RMIServer.PORT);
+            Remote stub = UnicastRemoteObject.exportObject(server, 0);
+            registry.bind(RMIServer.UNIQUE_BINDING_NAME, stub);
+
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (RemoteException | AlreadyBoundException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void jmsServer(DAO dao) {
+        var server = new AMQServer(dao);
+        while (!server.isDidUserExit()) Thread.onSpinWait();
     }
 }
