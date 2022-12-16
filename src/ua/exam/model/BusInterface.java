a42f66ca391cc539e47faf7a1df22573d033e0cb
chabanyukm <chabanyuk.maks@gmail.com>
2022-12-16 13:41:54 +0200

    interface for bus created

---
+++ src/ua/exam/model/BusInterface.java
@@ -0,0 +1,34 @@
+package ua.exam.model;
+
+import java.util.List;
+
+public interface BusInterface {
+
+    String getDriverName();
+
+    int getBusNumber();
+
+    int getRouteNumber();
+
+    String getBrand();
+
+    int getManufacturedYear();
+
+    double getMileAge();
+
+    void setDriverName(String newName);
+
+    void setBusNumber(int newNumber);
+
+    void setRouteNumber(int newNumber);
+
+    void setBrand(String newBrand);
+
+    void setManufacturedYear(int newYear);
+
+    void setMileAge(double newMileAge);
+
+    List<BusInterface> randomListOfBuses(int n);
+
+    List<BusInterface> getFixedListOfBuses();
+}
