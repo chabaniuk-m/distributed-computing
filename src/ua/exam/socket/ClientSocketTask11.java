0cb5ac28ce853ca64f28a7e533bd1ad973acc6be
chabanyukm <chabanyuk.maks@gmail.com>
2022-12-16 16:09:47 +0200

    final Socket communication

--- src/ua/exam/socket/ClientSocketTask11.java
+++ src/ua/exam/socket/ClientSocketTask11.java
@@ -1,4 +1,149 @@
 package ua.exam.socket;
 
+import ua.exam.rmi.ClientRmiTask11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+import static ua.exam.rmi.ClientRmiTask11.printCommands;
+
 public class ClientSocketTask11 {
+    private final BufferedReader in;
+    private final PrintWriter out;
+
+    public ClientSocketTask11() {
+        try {
+            Socket socket = new Socket(ServerSocketTask11.HOST, ServerSocketTask11.PORT);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            begin();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ClientSocketTask11();
+    }
+
+    private void begin() throws RemoteException, IOException {
+        System.out.println("List of all commands:");
+        printCommands();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            String command = scanner.nextLine();
+            if (command.length() == 0) continue;
+            switch (command.charAt(0)) {
+                case 's' -> {
+                    out.println('s');
+                    int count = Integer.parseInt(in.readLine());
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(in.readLine());
+                    }
+                }
+                case 'g' -> {
+                    if (command.length() == 1 || command.charAt(1) != ' ') ClientRmiTask11.invalidCommand(command);
+                    else {
+                        int n = 0;
+                        try {
+                            n = Integer.parseInt(command.substring(1).trim());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please enter a whole number after a space");
+                            continue;
+                        }
+                        if (n <= 0) {
+                            System.out.println("Please enter a positive number");
+                        } else {
+                            out.println("g" + n);
+                            System.out.println(in.readLine());
+                        }
+                    }
+                }
+                case 'f' -> {
+                    out.println('f');
+                    System.out.println(in.readLine());
+                }
+                case 'r' -> {
+                    if (command.length() == 1 || command.charAt(1) != ' ') ClientRmiTask11.invalidCommand(command);
+                    else {
+                        int n = 0;
+                        try {
+                            n = Integer.parseInt(command.substring(1).trim());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please enter a whole number after a space");
+                            continue;
+                        }
+                        if (n <= 0) {
+                            System.out.println("Please enter a positive number");
+                        } else {
+                            out.println("r" + n);
+                            int count = Integer.parseInt(in.readLine());
+                            for (int i = 0; i < count; i++) {
+                                System.out.println(in.readLine());
+                            }
+                        }
+                    }
+                }
+                case 'y' -> {
+                    if (command.length() == 1 || command.charAt(1) != ' ') ClientRmiTask11.invalidCommand(command);
+                    else {
+                        int n = 0;
+                        try {
+                            n = Integer.parseInt(command.substring(1).trim());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please enter a whole number after a space");
+                            continue;
+                        }
+                        if (n <= 0) {
+                            System.out.println("Please enter a positive number");
+                        } else {
+                            out.println("y" + n);
+                            int count = Integer.parseInt(in.readLine());
+                            for (int i = 0; i < count; i++) {
+                                System.out.println(in.readLine());
+                            }
+                        }
+                    }
+                }
+                case 'm' -> {
+                    if (command.length() == 1 || command.charAt(1) != ' ') ClientRmiTask11.invalidCommand(command);
+                    else {
+                        double n = 0;
+                        try {
+                            n = Double.parseDouble(command.substring(1).trim());
+                        } catch (NumberFormatException e) {
+                            System.out.println("Please enter a whole number after a space");
+                            continue;
+                        }
+                        if (n < 0) {
+                            System.out.println("Please enter a non-negative number");
+                        } else {
+                            out.println("m" + n);
+                            int count = Integer.parseInt(in.readLine());
+                            for (int i = 0; i < count; i++) {
+                                System.out.println(in.readLine());
+                            }
+                        }
+                    }
+                }
+                case 'e' -> {
+                    try {
+                        out.println('e');
+                        return;
+                    } catch (Exception e) {
+                        System.out.println("Thank you for using our service!");
+                        return;
+                    }
+                }
+                default -> ClientRmiTask11.invalidCommand(command);
+            }
+        }
+    }
 }
--- src/ua/exam/socket/ServerSocketTask11.java
+++ src/ua/exam/socket/ServerSocketTask11.java
@@ -1,4 +1,153 @@
 package ua.exam.socket;
 
+import ua.exam.model.Bus;
+import ua.exam.model.BusInterface;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerSocketTask11 {
+    public static final String HOST = "localhost";
+    public static final int PORT = 10352;
+    private final BufferedReader in;
+    private final PrintWriter out;
+    private final ServerSocket server;
+    private final Socket client;
+    private boolean fixed = false;
+    private List<BusInterface> buses = null;
+
+    public static void main(String[] args) {
+        new ServerSocketTask11();
+    }
+
+    public ServerSocketTask11() {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Waiting for client...");
+            client = server.accept();
+            System.out.println("Client is connected");
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+            begin();
+            server.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void begin() {
+        while (true) {
+            String command = readLine();
+            switch (command.charAt(0)) {
+                case 'g' -> g(Integer.parseInt(command.substring(1)));
+                case 'f' -> f();
+                case 's' -> s();
+                case 'r' -> r(Integer.parseInt(command.substring(1)));
+                case 'y' -> y(Integer.parseInt(command.substring(1)));
+                case 'm' -> m(Double.parseDouble(command.substring(1)));
+                case 'e' -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    private void g(int n) {
+        fixed = false;
+        buses = Bus.b.randomListOfBuses(n);
+        out.println(n + " buses are generated");
+    }
+
+    private void f() {
+        if (!fixed) {
+            buses = Bus.b.getFixedListOfBuses();
+            fixed = true;
+            out.println(10 + " buses are generated");
+        } else {
+            out.println("The list of 10 buses have been already set.");
+        }
+    }
+
+    private void s() {
+        if (buses == null || buses.isEmpty()) {
+            out.println(1);
+            out.println("<no buses>");
+        } else {
+            out.println(buses.size());
+            out.println(String.join("\n", buses.stream().map(BusInterface::toString).toList()));
+        }
+    }
+
+    private void r(int n) {
+        if (buses == null) {
+            out.println(1);
+            out.println("‼️Список автобусів ще ні ініціалізований");
+            return;
+        }
+        List<BusInterface> onRoute = buses.stream().filter(b -> b.getRouteNumber() == n).toList();
+        if (onRoute.isEmpty()) {
+            out.println(1);
+            out.println("<no buses>");
+        }
+        else {
+            out.println(onRoute.size());
+            out.println(String.join("\n", onRoute.stream().map(BusInterface::toString).toList()));
+        }
+    }
+
+    private void y(int term) {
+        if (buses == null) {
+            out.println(1);
+            out.println("‼️Список автобусів ще ні ініціалізований");
+            return;
+        }
+        LocalDateTime now = LocalDateTime.now();
+        List<BusInterface> res = new ArrayList<>();
+        for (BusInterface bus : buses) {
+            LocalDateTime exploitStarted = LocalDateTime.of(bus.getManufacturedYear(), Month.JANUARY, 1, 1, 1, 1);
+            if (ChronoUnit.YEARS.between(exploitStarted, now) > term) {
+                res.add(bus);
+            }
+        }
+        if (res.isEmpty()) {
+            out.println(1);
+            out.println("<no buses>");
+        } else {
+            out.println(res.size());
+            out.println(String.join("\n", res.stream().map(BusInterface::toString).toList()));
+        }
+    }
+
+    private void m(double mileage) {
+        if (buses == null) {
+            out.println(1);
+            out.println("‼️Список автобусів ще ні ініціалізований");
+            return;
+        }
+        List<BusInterface> greaterMileage = buses.stream().filter(b -> b.getMileAge() > mileage).toList();
+        if (buses.isEmpty()) {
+            out.println(1);
+            out.println("<no buses>");
+        } else {
+            out.println(greaterMileage.size());
+            out.println(String.join("\n", greaterMileage.stream().map(BusInterface::toString).toList()));
+        }
+    }
+
+    private String readLine() {
+        try {
+            return in.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
